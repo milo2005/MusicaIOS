1f877224f34a4f8a4d6c04a32fc2184f9a7de302
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-01-30 16:17:34 -0500

    Initial Commit

---
+++ Musica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Musica
+//
+//  Created by Estacion ViveLab Training Center 014 on 30/01/16.
+//  Copyright © 2016 Estacion ViveLab Training Center 014. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
